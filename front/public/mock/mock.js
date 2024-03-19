7bd6798ddbdf541bb4680c4333efd6655ec7c5a1
백현영 <116708517+DOKIDOKI-b@users.noreply.github.com>
2024-03-19 17:01:48 +0900

    feat : feed fetch시 사용할 mock data

---
+++ front/public/mock/mock.js
@@ -0,0 +1,5 @@
+export const mock = Array.from({ length: 30 }, () => ({
+  imageUrl: 'https://via.placeholder.com/200x140',
+  content:
+    '안녕하세요 반갑습니다 안녕히가세요안녕히가세요안녕히가세요안녕히가세요안녕히가세요안녕히가세요안녕히가세요안녕히가세요안녕히가세요안녕히가세요안녕히가세요안녕히가세요안녕히가세요안녕히가세요',
+}));
