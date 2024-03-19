2a8782726b690198e38952f568b10baaf4975270
백현영 <116708517+DOKIDOKI-b@users.noreply.github.com>
2024-03-19 17:05:03 +0900

    feat : 메인페이지

---
+++ front/modules/index.js
@@ -0,0 +1,32 @@
+import { $ } from '../src/utils/dom.js';
+import { mock } from '../public/mock/mock.js';
+
+const appendCards = () => {
+  const $cardList = $('.card-list');
+
+  const cardsHTML = mock
+    .map(
+      ({ imageUrl, content }) =>
+        `
+        <div class="rounded-lg shadow-lg w-[240px] border p-[24px]">
+        <div class="flex flex-col items-center gap-[16px]">
+          <img
+            src="${imageUrl}"
+            alt="Image"
+            class="border rounded-[8px] object-fit"
+          />
+            <p class="multi-ellipsis h-[100px] multi-ellipsis font-semibold">
+            ${content}
+            </p>
+        </div>
+      </div>
+  `
+    )
+    .join('');
+
+  $cardList.innerHTML = cardsHTML;
+};
+
+document.addEventListener('DOMContentLoaded', () => {
+  appendCards();
+});
