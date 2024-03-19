3f426b1c5e22873db8c87c4d8c32276be97000ee
chenjingdev <chenjingdev@gmail.com>
2024-03-19 17:53:45 +0900

    들여쓰기 수정

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ db = client.jungle
 # HTML 화면 보여주기
 @app.route("/")
 def home():
-    return render_template("index.html", name="테스터")
+	return render_template("index.html", name="테스터")
 
 # @app.route('/<name>')
 # def hello(name):
@@ -24,59 +24,59 @@ SECRET_KEY = 'apple'
 # 회원가입
 @app.route("/signin", methods=["POST"])
 def signin_proc():
-    # 요청 내용 파싱
-    print(request.form)
-    input_data = request.form
-    user_id = input_data["user_id"]
-    user_password = input_data["user_password"]
-    user_name = input_data["user_name"]
+	# 요청 내용 파싱
+	print(request.form)
+	input_data = request.form
+	user_id = input_data["user_id"]
+	user_password = input_data["user_password"]
+	user_name = input_data["user_name"]
 
-    # 아이디 중복 조회
-    find_user = db.users.find_one({"user_id": user_id})
-    print(find_user)
+	# 아이디 중복 조회
+	find_user = db.users.find_one({"user_id": user_id})
+	print(find_user)
 
-    user = {"user_id": user_id, "user_password": user_password, "user_name": user_name}
-    # return jsonify({'result': 'success', 'user': user})
-    # 중복되는 아이디가 없을경우
-    if find_user == None:
-        print("중복 아이디 없음")
-        db.users.insert_one(user)
-        return jsonify({"result": "success", "message": "등록 완료"})
+	user = {"user_id": user_id, "user_password": user_password, "user_name": user_name}
+	# return jsonify({'result': 'success', 'user': user})
+	# 중복되는 아이디가 없을경우
+	if find_user == None:
+			print("중복 아이디 없음")
+			db.users.insert_one(user)
+			return jsonify({"result": "success", "message": "등록 완료"})
 
-    # 아이디, 비밀번호가 일치하지 않는 경우
-    else:
-        return jsonify({"result": "fail", "message": "아이디 중복됨"})
+	# 아이디, 비밀번호가 일치하지 않는 경우
+	else:
+			return jsonify({"result": "fail", "message": "아이디 중복됨"})
 
 
 # 로그인
 @app.route("/login", methods=["POST"])
 def login_proc():
-    # 요청 내용 파싱
-    print(request.form)
-    input_data = request.form
-    user_id = input_data["user_id"]
-    user_password = input_data["user_password"]
+	# 요청 내용 파싱
+	print(request.form)
+	input_data = request.form
+	user_id = input_data["user_id"]
+	user_password = input_data["user_password"]
 
-    # 가입 여부 확인
-    find_user = db.users.find_one({"user_id": user_id})
-    print(find_user)
+	# 가입 여부 확인
+	find_user = db.users.find_one({"user_id": user_id})
+	print(find_user)
 
-	  # 아이디, 비밀번호가 일치하는 경우
-    if (find_user == None): # 아이디 없음
-      print("미가입 아이디")
-      return jsonify({'result': 'fail','message':'미가입 아이디'})
-      # payload = {
-      # 	'id': user_id,
-      # 	'exp': datetime.utcnow() + timedelta(seconds=60)  # 로그인 24시간 유지
-      # }
-      # token = jwt.encode(payload, SECRET_KEY, algorithm='HS256')
+	# 아이디, 비밀번호가 일치하는 경우
+	if (find_user == None): # 아이디 없음
+		print("미가입 아이디")
+		return jsonify({'result': 'fail','message':'미가입 아이디'})
+		# payload = {
+		# 	'id': user_id,
+		# 	'exp': datetime.utcnow() + timedelta(seconds=60)  # 로그인 24시간 유지
+		# }
+		# token = jwt.encode(payload, SECRET_KEY, algorithm='HS256')
 
-    elif (find_user['user_id'] == user_id and find_user['user_password'] == user_password):
-      
-      return jsonify({'result': 'success', 'message': '로그인 성공'})
-    # 아이디, 비밀번호가 일치하지 않는 경우
-    else:
-      return jsonify({'result': 'fail'})
+	elif (find_user['user_id'] == user_id and find_user['user_password'] == user_password):
+		
+		return jsonify({'result': 'success', 'message': '로그인 성공'})
+	# 아이디, 비밀번호가 일치하지 않는 경우
+	else:
+		return jsonify({'result': 'fail'})
 
 
 
@@ -119,4 +119,4 @@ def login_proc():
 
 
 if __name__ == "__main__":
-    app.run("0.0.0.0", port=5001, debug=True)
+	app.run("0.0.0.0", port=5001, debug=True)
