142c99d47bc915aa00530d96e95f6191855d20c4
백현영 <116708517+DOKIDOKI-b@users.noreply.github.com>
2024-03-20 14:16:41 +0900

    feat : 로그인 이후 쿠키에 token과 nick name 저장

--- static/front/modules/logInPage.js
+++ static/front/modules/logInPage.js
@@ -5,12 +5,19 @@ document.addEventListener('DOMContentLoaded', () => {
   const $loginForm = $('.log-in-form');
   const $loginBtn = $('.log-in-btn');
 
-  $loginBtn.addEventListener('click', (e) => {
+  $loginBtn.addEventListener('click', async (e) => {
     e.preventDefault();
 
     const formData = new FormData($loginForm);
     const [user_id, user_password] = formData.values();
 
-    apiService.logIn({ user_id, user_password });
+    const res = await apiService.login({
+      id: user_id,
+      password: user_password,
+    });
+
+    const { token, userName } = res;
+
+    document.cookie = encodeURIComponent(`token=${token}; name=${userName};`);
   });
 });
