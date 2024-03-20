2a4b54ed324ed5163fc4769eae5957f9d2bcd15a
백현영 <116708517+DOKIDOKI-b@users.noreply.github.com>
2024-03-20 14:17:44 +0900

    feat : 로그인 기능 구현, 피드 받아오기 기능

--- static/front/modules/ApiService.js
+++ static/front/modules/ApiService.js
@@ -1,39 +1,38 @@
+const FAIL = 'fail';
 class ApiService {
   #BASE_URL = 'http://192.168.1.175:5000';
 
-  // async login({ id, password }) {
-  //   const res = await fetch(this.#BASE_URL, {
-  //     method: 'POST',
-  //     body: {
-  //       id,
-  //       password,
-  //     },
-  //   });
-
-  //   const data = res.json();
-  //   // 400 -> error
-  //   // 200 -> success
-  //   // 추가처리...
-  // }
-
-  async fetchFeed() {
+  async login({ id, password }) {
     try {
-      const res = await fetch(`${this.#BASE_URL}/api/feed`, {
-        method: 'GET',
+      const res = await fetch(`${this.#BASE_URL}/api/login`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          user_id: id,
+          user_password: password,
+        }),
       });
 
       if (res.ok) {
         const data = await res.json();
+        const { result } = data;
 
-        return data.message;
+        if (result === FAIL) {
+          this.#failLogin(data.message);
+        } else {
+          const { user_name, token } = data;
+
+          return {
+            userName: user_name,
+            token,
+          };
+        }
       }
-
-      alert('피드 가져오기 실패');
     } catch (error) {
-      alert('네트워크 오류가 발생했습니다.');
+      alert('서버에 문제가 생겼습니다.');
     }
-
-    return feeds;
   }
 
   async signIn({ id, password, password_check, nick_name }) {
@@ -58,8 +57,31 @@ class ApiService {
       return data;
     } catch (error) {
       alert('서버에 문제가 생겼습니다.');
-      console.log(res.status);
     }
+  }
+
+  async fetchFeed() {
+    try {
+      const res = await fetch(`${this.#BASE_URL}/api/feed`, {
+        method: 'GET',
+      });
+
+      if (res.ok) {
+        const data = await res.json();
+
+        return data.message;
+      }
+
+      alert('피드 가져오기 실패');
+    } catch (error) {
+      alert('네트워크 오류가 발생했습니다.');
+    }
+
+    return feeds;
+  }
+
+  #failLogin(message) {
+    alert(message);
   }
 }
 
