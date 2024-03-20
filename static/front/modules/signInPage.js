721000810b4298e6c66e408f175aeff7869f2a05
백현영 <116708517+DOKIDOKI-b@users.noreply.github.com>
2024-03-20 11:39:50 +0900

    feat : 회원가입 성공시 login 페이지로 이동

--- static/front/modules/signInPage.js
+++ static/front/modules/signInPage.js
@@ -5,18 +5,11 @@ document.addEventListener('DOMContentLoaded', () => {
   const $signInBtn = $('.sign-in-btn');
   const $signInForm = $('.sign-in-form');
 
-  $signInBtn.addEventListener('click', (e) => {
+  $signInBtn.addEventListener('click', async (e) => {
     e.preventDefault();
 
     const formData = new FormData($signInForm);
     const [id, nick_name, password, password_check] = formData.values();
-
-    console.log(
-      [...formData].filter(([, val]) => {
-        console.log(val);
-        !val;
-      })
-    );
 
     if ([...formData].filter(([_, val]) => !val).length !== 0) {
       alert('모든 입력창에 값을 입력해 주세요.');
@@ -42,6 +35,8 @@ document.addEventListener('DOMContentLoaded', () => {
       return;
     }
 
-    apiService.signIn({ id, password, password_check, nick_name });
+    await apiService.signIn({ id, password, password_check, nick_name });
+
+    window.location.href = '/user/login';
   });
 });
