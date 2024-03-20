9921bab4a69708534fde6ee0326fbef8682eb4b7
백현영 <116708517+DOKIDOKI-b@users.noreply.github.com>
2024-03-20 10:07:44 +0900

    feat : 회원가입 구현

--- static/front/modules/ApiService.js
+++ static/front/modules/ApiService.js
@@ -1,49 +1,57 @@
 class ApiService {
-  #BASE_URL = 'https://192.168.1.175:5000';
+  #BASE_URL = 'http://192.168.1.175:5000';
 
-  // async login({ id, password }) {
-  //   const res = await fetch(this.#BASE_URL, {
-  //     method: 'POST',
-  //     body: {
-  //       id,
-  //       password,
-  //     },
-  //   });
-  //   const data = res.json();
-  //   // 400 -> error
-  //   // 200 -> success
-  //   // 추가처리...
-  // }
-
-  // async fetchFeed() {
-  //   try {
-  //     const res = await fetch(this.#BASE_URL);
-  //     const feeds = res.json();
-  //   } catch (error) {
-  //     if (error instanceof TypeError) {
-  //       alert('네트워크 오류가 발생했습니다.');
-  //     }
-  //   }
-
-  //   return feeds;
-  // }
-
-  async signIn({ id, password, password_check, nickName }) {
-    let body = {
-      user_id: id,
-      user_password: password,
-      user_name: nickName,
-    }
-    fetch('http://192.168.1.175:5000/api/signup', {
+  async login({ id, password }) {
+    const res = await fetch(this.#BASE_URL, {
       method: 'POST',
-      headers: {
-        "Content-Type": "application/json",
+      body: {
+        id,
+        password,
       },
-      body : JSON.stringify(body),
-    })
-  .then((response) => console.log("response:", response.message))
-  .catch((error) => console.log("error:", error));
-    //console.log(data);
+    });
+    const data = res.json();
+    // 400 -> error
+    // 200 -> success
+    // 추가처리...
+  }
+
+  async fetchFeed() {
+    try {
+      const res = await fetch(this.#BASE_URL);
+      const feeds = res.json();
+    } catch (error) {
+      if (error instanceof TypeError) {
+        alert('네트워크 오류가 발생했습니다.');
+      }
+    }
+
+    return feeds;
+  }
+
+  async signIn({ id, password, password_check, nick_name }) {
+    try {
+      const res = await fetch(`${this.#BASE_URL}/api/signup`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          user_id: id,
+          user_password: password,
+          user_name: nick_name,
+        }),
+      });
+      const data = await res.json();
+
+      if (res.ok) {
+        alert('회원가입 완료 !');
+      }
+
+      return data;
+    } catch (error) {
+      alert('서버에 문제가 생겼습니다.');
+      console.log(res.status);
+    }
   }
 }
 
