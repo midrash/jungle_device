5a7c2ae8b3a2f41f84716bd3cb07d971e11d30b7
백현영 <116708517+DOKIDOKI-b@users.noreply.github.com>
2024-03-20 10:23:12 +0900

    feat : 회원가입 페이지 미입력 input 존재시 해당 el로 focus 기능 추가

--- static/front/modules/signInPage.js
+++ static/front/modules/signInPage.js
@@ -9,9 +9,39 @@ document.addEventListener('DOMContentLoaded', () => {
     e.preventDefault();
 
     const formData = new FormData($signInForm);
-    const [id, nickName, password, password_check] = formData.values();
+    const [id, nick_name, password, password_check] = formData.values();
 
-    console.log(id, nickName, password, password_check);
-    apiService.signIn({ id, password, password_check, nickName });
+    console.log(
+      [...formData].filter(([, val]) => {
+        console.log(val);
+        !val;
+      })
+    );
+
+    if ([...formData].filter(([_, val]) => !val).length !== 0) {
+      alert('모든 입력창에 값을 입력해 주세요.');
+
+      const notFillInput = [...formData]
+        .filter(([_, val]) => !val)
+        .map(([key, val]) => key)
+        .join('');
+
+      $(
+        `#${
+          notFillInput.indexOf('_')
+            ? notFillInput
+            : notFillInput.replace('_', '-')
+        }`
+      ).focus();
+
+      return;
+    }
+
+    if (password !== password_check) {
+      alert('동일한 패스워드를 입력해 주세요.');
+      return;
+    }
+
+    apiService.signIn({ id, password, password_check, nick_name });
   });
 });
